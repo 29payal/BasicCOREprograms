0dc0d47b54cb6b36f0393e57802e62188e9f3533
Payal Sharma <https://github.com/29payal>
2022-08-31 09:20:30 -0700

    [payal] Welcome

---
+++ COREPrograms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COREPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BASIC CORE PROGRAMS!");
+        }
+    }
+}
