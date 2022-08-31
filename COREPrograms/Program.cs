9e22f33b754219c8b3279c381ea44c569d1869c9
Payal Sharma <https://github.com/29payal>
2022-08-31 10:16:19 -0700

    [payal] UC4-SwapTwoNumber

--- COREPrograms/Program.cs
+++ COREPrograms/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BASIC CORE PROGRAMS!");
-            Console.WriteLine("Choose option\n  1. Flip coin\t  2. Leap year\t 3. Poweroftwo");
+            Console.WriteLine("Choose option\n  1. Flip coin\t  2. Leap year\t 3. Poweroftwo\t  4. Swap two Number");
             int option = Convert.ToInt32(Console.ReadLine());  
             switch (option)
             {
@@ -17,6 +17,9 @@
                     break;
                 case 3:
                     PowerofTwo.TableofTwo();
+                    break;
+                case 4:
+                    SwapTwoNumber.SwappingNumber();
                     break;
                 default:        
                     Console.WriteLine("Wrong Choice ");
