f1115aa65528abd1d84219203f8141f1352a29f4
Payal Sharma <https://github.com/29payal>
2022-08-31 09:35:35 -0700

    [payal] UC1-FlipCoins

---
+++ COREPrograms/FlipCoins.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COREPrograms
+{
+    internal class FlipCoins
+    {
+        public static void CalculatePercentage()
+        {
+            int HeadCount = 0, TailCount = 0;
+            double HeadPercentage;
+            double TailPercentage;
+            Console.WriteLine("Please enter value for number");
+            int flipcoin = Convert.ToInt32(Console.ReadLine());
+
+            if (flipcoin > 0)
+            {
+                for (int i = 0; i < flipcoin; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);//0,1
+                    if (number ==1)
+                    {
+                        Console.WriteLine("Head");
+                        HeadCount++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tail");
+                        TailCount++;    
+                    }
+                }
+                HeadPercentage = HeadCount*100/ flipcoin;
+                TailPercentage = TailCount*100/ flipcoin;
+                Console.WriteLine("Head percentage: " + HeadPercentage+" "+"Tail percentage:" +TailPercentage);
+            }
+            else
+            {
+                Console.WriteLine("Enter positive number");
+            }
+        }
+    }
+}
--- COREPrograms/Program.cs
+++ COREPrograms/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BASIC CORE PROGRAMS!");
+            Console.WriteLine("Choose option\n  1. Flip coin");
+            int option = Convert.ToInt32(Console.ReadLine());  
+            switch (option)
+            {
+                case 1:
+                    FlipCoins.CalculatePercentage();
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choice ");
+                    break;
+
+            }
         }
     }
 }
