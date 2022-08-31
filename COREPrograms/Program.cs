8a0a609f03f812e2c620161179c5ea79c713a564
Payal Sharma <https://github.com/29payal>
2022-08-31 11:26:13 -0700

    [payal] UC8-QuotientRemainder

--- COREPrograms/Program.cs
+++ COREPrograms/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BASIC CORE PROGRAMS!");
-            Console.WriteLine("Choose option\n  1. Flip coin\t  2. Leap year\t 3. Poweroftwo\t  4. Swap two Number\t  5. EverorOdd\t 6.VowelorConsonant\t 7. Largest almost three number");
+            Console.WriteLine("Choose option\n  1. Flip coin\t  2. Leap year\t 3. Poweroftwo\t  4. Swap two Number\t  5. EverorOdd\t 6.VowelorConsonant\t 7. Largest almost three number\t 8.Quotient and Remainder");
             int option = Convert.ToInt32(Console.ReadLine());  
             switch (option)
             {
@@ -29,6 +29,9 @@
                     break;
                 case 7:
                     LargestNumber.LargestNo();
+                    break;
+                case 8:
+                    QuotientRemainder.QuotRemain();
                     break;
                 default:        
                     Console.WriteLine("Wrong Choice ");
