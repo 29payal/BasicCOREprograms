bdce822ac34471a7c351da110487c76cabaf7cd7
Payal Sharma <https://github.com/29payal>
2022-08-31 10:01:25 -0700

    [payal] UC3-PowerofTwo

---
+++ COREPrograms/PowerofTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COREPrograms
+{
+    internal class PowerofTwo
+    {
+        public static void TableofTwo()
+        {
+            int j, n;
+            Console.WriteLine("Input the number");
+            n= Convert.ToInt32(Console.ReadLine());
+            for (j = 1; j <= 10; j++) 
+            {
+                Console.WriteLine( "{0} * {1} = {2}", n,j,n*j);
+            }
+        }
+    }
+}
--- COREPrograms/Program.cs
+++ COREPrograms/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BASIC CORE PROGRAMS!");
-            Console.WriteLine("Choose option\n  1. Flip coin\t  2. Leap yaer");
+            Console.WriteLine("Choose option\n  1. Flip coin\t  2. Leap year\t 3. Poweroftwo");
             int option = Convert.ToInt32(Console.ReadLine());  
             switch (option)
             {
@@ -14,6 +14,9 @@
                     break;
                     case 2: 
                         LeapYear.CheckLeapYear();
+                    break;
+                case 3:
+                    PowerofTwo.TableofTwo();
                     break;
                 default:        
                     Console.WriteLine("Wrong Choice ");
