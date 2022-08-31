26ea3fcce44a12fb61cac9b69703872b258b9514
Payal Sharma <https://github.com/29payal>
2022-08-31 11:49:58 -0700

    [payal] UC9-HarmonicNo

---
+++ COREPrograms/HarmonicNo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COREPrograms
+{
+    internal class HarmonicNo
+    {
+        public static void HarmonicNum()
+        {
+            int n, i;
+            double result = 0.0;
+            Console.WriteLine("Enter the vaule for harmonic ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                result+=1/(float)i;
+                Console.WriteLine("1/{0} +", i);
+            }
+            Console.WriteLine($"={result}");
+        }
+    }
+}
--- COREPrograms/Program.cs
+++ COREPrograms/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BASIC CORE PROGRAMS!");
-            Console.WriteLine("Choose option\n  1. Flip coin\t  2. Leap year\t 3. Poweroftwo\t  4. Swap two Number\t  5. EverorOdd\t 6.VowelorConsonant\t 7. Largest almost three number\t 8.Quotient and Remainder");
+            Console.WriteLine("Choose option\n  1. Flip coin\t  2. Leap year\t 3. Poweroftwo\t  4. Swap two Number\t  5. EverorOdd\t 6.VowelorConsonant\t 7. Largest almost three number\t 8.Quotient and Remainder\t 9. Harmonic Number");
             int option = Convert.ToInt32(Console.ReadLine());  
             switch (option)
             {
@@ -32,6 +32,9 @@
                     break;
                 case 8:
                     QuotientRemainder.QuotRemain();
+                    break;
+                case 9:
+                    HarmonicNo.HarmonicNum();
                     break;
                 default:        
                     Console.WriteLine("Wrong Choice ");
